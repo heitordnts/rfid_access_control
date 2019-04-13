7a3c923553f77b4dec1cd512747135b3c4306055
HeitorDantas <heitor.dantas17@gmail.com>
2019-04-13 12:27:26 -0300

    todo list

--- main.c
+++ main.c
@@ -1,3 +1,41 @@
+/*
+	Controle de acesso com RFID - Sistemas digitais - UFRN
+	Professor: Sergio Natan
+	componentes:
+		-	Heitor Dantas
+		-	Mateus Rodrigues	
+		-	Luiza  Salviano
+
+	FUNCIONAMENTO:
+		* leds de sinalizacao
+		Se o sensor ultrassonico acusa presenca
+			a cancela 1 (motor 1) eh aberta
+			espera o carro passar
+			fecha cancela 1
+			
+			*flag pra indicar que existe um carro na gaiola
+
+			verificacao RFID
+				caso possua id valido:
+					rotina de abrir cancela 2
+					guarda num buffer o ID que entrou
+			
+
+			interrupcao de contagem para mandar o buffer para arquivo
+			de log usando USART; 
+
+	TODO:
+		(ok) logica da chave
+		()   sensor ultrassonico
+		() 	 verificacao rfid - estrutura pronta falta integrar no programa
+		()   rotina para acionamento dos motores 
+		()   arquivo de log
+
+
+*/
+
+
+
 #include <avr/io.h>
 
 #define FOSC 16000000 // Clock Speed
@@ -20,7 +58,7 @@ void usart_init( unsigned int ubrr)
 	/*Set baud rate */
 	UBRR0H = (unsigned char)(ubrr>>8);
 	UBRR0L = (unsigned char)ubrr;
-	Enable receiver and transmitter */
+	//Enable receiver and transmitter */
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
 	/* Set frame format: 8data, 2stop bit */
 	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
@@ -45,9 +83,12 @@ void setup() {
 	PORTB |= (1 << CHAVE);//pull up	
 	DDRD |= (1 << LED_1) | (1 << LED_2);
 	DDRD |= (1 << MOTOR_1) | (1 << MOTOR_2);	
+
 	//sensor Ultrassonico
 	//Ultrassonic();
+	usart_init(MYUBRR);
 } 
+
 int main(){
 
 	setup();
@@ -55,9 +96,10 @@ int main(){
 	while(1){
 		if(PINB & (1 << CHAVE)){ 
 			PORTD |= (1 << LED_1);
+			
 		}
 		else{
-
+			
 			PORTD &= ~(1 << LED_1);
 		}
 	}
